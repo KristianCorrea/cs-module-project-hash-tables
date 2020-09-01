1da6ad220e0c571f9c1741c97e5ee1580cab3d0d
Kristian Correa <kristiangcorrea@live.com>
2020-09-01 18:53:12 -0400

    mvp

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -21,7 +21,9 @@ class HashTable:
     """
 
     def __init__(self, capacity):
-        # Your code here
+        self.capacity = capacity
+        self.storage = [None] * capacity
+        self.item_count = 0
 
 
     def get_num_slots(self):
@@ -34,7 +36,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        return len(self.storage)
 
 
     def get_load_factor(self):
@@ -43,7 +45,7 @@ class HashTable:
 
         Implement this.
         """
-        # Your code here
+        return self.item_count / self.capacity
 
 
     def fnv1(self, key):
@@ -62,7 +64,16 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        str_key = str(key).encode()
+
+        hash_value = 5381
+
+        for b in str_key:
+            hash_value = ((hash_value << 5) + hash_value) + b
+        
+        hash_value &= 0xffffffff
+
+        return hash_value
 
 
     def hash_index(self, key):
@@ -82,6 +93,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        self.storage[index] = value
 
 
     def delete(self, key):
@@ -94,6 +107,9 @@ class HashTable:
         """
         # Your code here
 
+        index = self.hash_index(key)
+        self.storage[index] = None
+
 
     def get(self, key):
         """
@@ -105,6 +121,8 @@ class HashTable:
         """
         # Your code here
 
+        index = self.hash_index(key)
+        return self.storage[index]
 
     def resize(self, new_capacity):
         """
